87800975e83c93c8e430de5f6b6a78de28b83711
Danny Su <dsc@fb.com>
2023-03-27 23:38:39 -0700

    Add logic for handling keyboard input

--- src/index.js
+++ src/index.js
@@ -16,10 +16,12 @@ const textinput = document.getElementById('openai_prompt');
 
 let camera, scene, renderer;
 
-let room;
+let controller1, controller2;
 
 init();
 animate();
+
+let userPrompt;
 
 function init() {
     scene = new THREE.Scene();
@@ -27,13 +29,6 @@ function init() {
 
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
     camera.position.set(0, 1.6, 3);
-
-    room = new THREE.LineSegments(
-        new BoxLineGeometry(6, 6, 6, 10, 10, 10),
-        new THREE.LineBasicMaterial({ color: 0x808080 })
-    );
-    room.geometry.translate(0, 3, 0);
-    scene.add(room);
 
     //
 
@@ -50,29 +45,26 @@ function init() {
 
     //
 
+    function onSelect() {
+        if (document.activeElement.tagName === 'INPUT') {
+            textinput.blur();
+        } else {
+            textinput.focus();
+        }
+    }
+    controller1 = renderer.xr.getController(0);
+    controller1.addEventListener('selectend', onSelect);
+    controller2 = renderer.xr.getController(1);
+    controller2.addEventListener('selectend', onSelect);
+
+    //
+
     window.addEventListener('resize', onWindowResize);
 
     //
 
-    let functionCount = 0;
-    textinput.oninput = async function(value) {
-        const completion = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: `// a Three.js function named "func${functionCount}" that creates and returns ${value}`,
-            max_tokens: 3000,
-        });
-        functionCount++;
-        const code = completion.data.choices[0].text;
-        eval(code);
-        const objName = `obj${functionCount}`;
-        eval(`
-            const ${objName} = func${functionCount}();
-            scene.add(${objName});
-
-            // Put the object at a random place
-
-            ${objName}.position.set(1, 1, 1);
-        `);
+    textinput.oninput = function(inputEvent) {
+        userPrompt = inputEvent.data;
     };
 }
 
@@ -88,5 +80,12 @@ function animate() {
 }
 
 function render() {
+    if (document.activeElement.tagName !== 'INPUT') {
+        if (userPrompt) {
+            console.log('user prompt: ' + userPrompt);
+            userPrompt = null;
+        }
+    }
+
     renderer.render(scene, camera);
 }
